6f43bc71780b515f5f11c66037612012a7714fc9
daneil <daneil.dai@aceqilin.com>
2020-12-01 22:37:58 +0800

    init dist 全部放進來即可

---
+++ precache-manifest.3bdadbe7068a5bdbe75aa378e4fdc914.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f910e79d2f79ef83882a2e55bf1ada0f",
+    "url": "/css/aos.css"
+  },
+  {
+    "revision": "0387edfe79dd128830df",
+    "url": "/css/app.a05b21e2.css"
+  },
+  {
+    "revision": "f411c136e2bb302ada2120b3eb1d5bc3",
+    "url": "/css/bootstrap.min.css"
+  },
+  {
+    "revision": "07b1d2e69da8318a0953",
+    "url": "/css/chunk-vendors.c6add683.css"
+  },
+  {
+    "revision": "fcc1403d557279a157acdfe0b3fc4f5b",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "9f92061e59a623b270f1f0c36a451f5d",
+    "url": "/images/01.png"
+  },
+  {
+    "revision": "6feb4c61729beb169f9b2c6a4700ca8f",
+    "url": "/img/Lipstick/z001.jpg"
+  },
+  {
+    "revision": "d32bee1928baeb9ce5c45365f9761c6f",
+    "url": "/img/Lipstick/z002.jpg"
+  },
+  {
+    "revision": "2b787810fc9e2010c3a68f2156f1bd4a",
+    "url": "/img/Lipstick/z003.jpg"
+  },
+  {
+    "revision": "4f8cac6e2b445bb5baad289a9eb73b72",
+    "url": "/img/Lipstick/z004.png"
+  },
+  {
+    "revision": "134120a2d61dcdcca09043b3a371a283",
+    "url": "/img/Lipstick/z005.jpg"
+  },
+  {
+    "revision": "4f70a25afbd44647f97d84a60625bac3",
+    "url": "/img/Lipstick/z006.jpg"
+  },
+  {
+    "revision": "e60f8b52e279a566e98c8d1b949a20c9",
+    "url": "/img/Lipstick/z007.jpg"
+  },
+  {
+    "revision": "731c7e5fecea6077b26687b23dd67e85",
+    "url": "/img/Lipstick/z008.jpg"
+  },
+  {
+    "revision": "e9521a8e9e9577f5d6145e2500820c14",
+    "url": "/img/Lipstick/z009.jpg"
+  },
+  {
+    "revision": "908d82c3b27f8bf001d3ea72e04ae195",
+    "url": "/img/Lipstick/z010.jpg"
+  },
+  {
+    "revision": "fa9711a67fb02ee679d299f32e450e07",
+    "url": "/img/Lipstick/z011.jpg"
+  },
+  {
+    "revision": "3edab9eaf511da95b72cd7d61ceba9c6",
+    "url": "/img/Lipstick/z012.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4d060b06aeb782b18619363f0f1804fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf246499d0283bec9639",
+    "url": "/js/about.d2f094c2.js"
+  },
+  {
+    "revision": "a01f9089e8301e9eacfb9d029dc0ca5c",
+    "url": "/js/aos.js"
+  },
+  {
+    "revision": "0387edfe79dd128830df",
+    "url": "/js/app.075e0395.js"
+  },
+  {
+    "revision": "eb5fac582a82f296aeb74900b01a2fa3",
+    "url": "/js/bootstrap.min.js"
+  },
+  {
+    "revision": "642d6d4bd5e3abc4108f",
+    "url": "/js/chunk-2d0b68f8.cc4de240.js"
+  },
+  {
+    "revision": "29b73cdb25c8a97e9297",
+    "url": "/js/chunk-2d229481.dfef86cc.js"
+  },
+  {
+    "revision": "1b40525b2a4c02658397",
+    "url": "/js/chunk-5bd0b9aa.4eeacf91.js"
+  },
+  {
+    "revision": "4b1b6a11cfd47b5589a8",
+    "url": "/js/chunk-6473fc7e.dab0e8b1.js"
+  },
+  {
+    "revision": "d01ed831f201773db992",
+    "url": "/js/chunk-7173126a.ecd6d59e.js"
+  },
+  {
+    "revision": "07b1d2e69da8318a0953",
+    "url": "/js/chunk-vendors.ff989a1a.js"
+  },
+  {
+    "revision": "a09e13ee94d51c524b7e2a728c7d4039",
+    "url": "/js/jquery-3.3.1.min.js"
+  },
+  {
+    "revision": "83fb8c4d9199dce0224da0206423106f",
+    "url": "/js/popper.min.js"
+  },
+  {
+    "revision": "fe3b9bce8d930e0d5fb01aadacd1c01c",
+    "url": "/js/swiper.min.js"
+  },
+  {
+    "revision": "ef001587ca0a02e225f065ea15fd621f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ precache-manifest.68d8521f1b3131ba1f976ce1fa184580.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f910e79d2f79ef83882a2e55bf1ada0f",
+    "url": "/daidai-vue-spa/css/aos.css"
+  },
+  {
+    "revision": "c1f319b45a883cd0c84f",
+    "url": "/daidai-vue-spa/css/app.a05b21e2.css"
+  },
+  {
+    "revision": "f411c136e2bb302ada2120b3eb1d5bc3",
+    "url": "/daidai-vue-spa/css/bootstrap.min.css"
+  },
+  {
+    "revision": "07b1d2e69da8318a0953",
+    "url": "/daidai-vue-spa/css/chunk-vendors.c6add683.css"
+  },
+  {
+    "revision": "fcc1403d557279a157acdfe0b3fc4f5b",
+    "url": "/daidai-vue-spa/css/style.css"
+  },
+  {
+    "revision": "9f92061e59a623b270f1f0c36a451f5d",
+    "url": "/daidai-vue-spa/images/01.png"
+  },
+  {
+    "revision": "6feb4c61729beb169f9b2c6a4700ca8f",
+    "url": "/daidai-vue-spa/img/Lipstick/z001.jpg"
+  },
+  {
+    "revision": "d32bee1928baeb9ce5c45365f9761c6f",
+    "url": "/daidai-vue-spa/img/Lipstick/z002.jpg"
+  },
+  {
+    "revision": "2b787810fc9e2010c3a68f2156f1bd4a",
+    "url": "/daidai-vue-spa/img/Lipstick/z003.jpg"
+  },
+  {
+    "revision": "4f8cac6e2b445bb5baad289a9eb73b72",
+    "url": "/daidai-vue-spa/img/Lipstick/z004.png"
+  },
+  {
+    "revision": "134120a2d61dcdcca09043b3a371a283",
+    "url": "/daidai-vue-spa/img/Lipstick/z005.jpg"
+  },
+  {
+    "revision": "4f70a25afbd44647f97d84a60625bac3",
+    "url": "/daidai-vue-spa/img/Lipstick/z006.jpg"
+  },
+  {
+    "revision": "e60f8b52e279a566e98c8d1b949a20c9",
+    "url": "/daidai-vue-spa/img/Lipstick/z007.jpg"
+  },
+  {
+    "revision": "731c7e5fecea6077b26687b23dd67e85",
+    "url": "/daidai-vue-spa/img/Lipstick/z008.jpg"
+  },
+  {
+    "revision": "e9521a8e9e9577f5d6145e2500820c14",
+    "url": "/daidai-vue-spa/img/Lipstick/z009.jpg"
+  },
+  {
+    "revision": "908d82c3b27f8bf001d3ea72e04ae195",
+    "url": "/daidai-vue-spa/img/Lipstick/z010.jpg"
+  },
+  {
+    "revision": "fa9711a67fb02ee679d299f32e450e07",
+    "url": "/daidai-vue-spa/img/Lipstick/z011.jpg"
+  },
+  {
+    "revision": "3edab9eaf511da95b72cd7d61ceba9c6",
+    "url": "/daidai-vue-spa/img/Lipstick/z012.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/daidai-vue-spa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d74eecdb796bfec70cf9a060e06208ae",
+    "url": "/daidai-vue-spa/index.html"
+  },
+  {
+    "revision": "bf246499d0283bec9639",
+    "url": "/daidai-vue-spa/js/about.d2f094c2.js"
+  },
+  {
+    "revision": "a01f9089e8301e9eacfb9d029dc0ca5c",
+    "url": "/daidai-vue-spa/js/aos.js"
+  },
+  {
+    "revision": "c1f319b45a883cd0c84f",
+    "url": "/daidai-vue-spa/js/app.126d1cc5.js"
+  },
+  {
+    "revision": "eb5fac582a82f296aeb74900b01a2fa3",
+    "url": "/daidai-vue-spa/js/bootstrap.min.js"
+  },
+  {
+    "revision": "642d6d4bd5e3abc4108f",
+    "url": "/daidai-vue-spa/js/chunk-2d0b68f8.cc4de240.js"
+  },
+  {
+    "revision": "29b73cdb25c8a97e9297",
+    "url": "/daidai-vue-spa/js/chunk-2d229481.dfef86cc.js"
+  },
+  {
+    "revision": "1b40525b2a4c02658397",
+    "url": "/daidai-vue-spa/js/chunk-5bd0b9aa.4eeacf91.js"
+  },
+  {
+    "revision": "4b1b6a11cfd47b5589a8",
+    "url": "/daidai-vue-spa/js/chunk-6473fc7e.dab0e8b1.js"
+  },
+  {
+    "revision": "d01ed831f201773db992",
+    "url": "/daidai-vue-spa/js/chunk-7173126a.ecd6d59e.js"
+  },
+  {
+    "revision": "07b1d2e69da8318a0953",
+    "url": "/daidai-vue-spa/js/chunk-vendors.ff989a1a.js"
+  },
+  {
+    "revision": "a09e13ee94d51c524b7e2a728c7d4039",
+    "url": "/daidai-vue-spa/js/jquery-3.3.1.min.js"
+  },
+  {
+    "revision": "83fb8c4d9199dce0224da0206423106f",
+    "url": "/daidai-vue-spa/js/popper.min.js"
+  },
+  {
+    "revision": "fe3b9bce8d930e0d5fb01aadacd1c01c",
+    "url": "/daidai-vue-spa/js/swiper.min.js"
+  },
+  {
+    "revision": "ef001587ca0a02e225f065ea15fd621f",
+    "url": "/daidai-vue-spa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/daidai-vue-spa/robots.txt"
+  }
+]);
